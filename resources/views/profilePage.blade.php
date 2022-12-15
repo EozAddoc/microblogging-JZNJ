779ea35a593dcdfc7507f6574d802ffde07b9d0d
Zoe Codda <addoceoz@MacBook-Air-de-Zoe.local>
2022-12-15 14:53:22 +0100

    jsp

--- resources/views/profilePage.blade.php
+++ resources/views/profilePage.blade.php
@@ -213,7 +213,7 @@ $count2 = Follow::where('follower_id', $userId)->count();
                           <form method="post" action="{{route('dashboard.likePost')}}" enctype="multipart/form-data" accept-charset="UTF-8">
                             {{ csrf_field()}}
                             <button type="submit"> <svg class="heart text-gray-700 mr-1 inline-block h-7 w-7  " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="myTest" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                               </svg></button>
                             {{$likes}}
                             <input type="hidden" name="post_id" value="{{ $post->id }}" />
@@ -261,6 +261,10 @@ $count2 = Follow::where('follower_id', $userId)->count();
   .menu-item {
     color: #FCC;
     padding: 3px;
+  }
+
+  #myTest:hover{
+    fill:red;
   }
 
   .three-dots:after {
